8a753655929eab8e1ebbd52dfa6bc892dbe26076
south-daria <jy.nam@weversecompany.com>
2022-09-01 14:55:02 +0900

    qr-code 컬러 넣을 수 있도록 수정

--- qr-code-generator.py
+++ qr-code-generator.py
@@ -6,9 +6,17 @@ f = open('user-info.csv', 'r')
 rdr = csv.reader(f)
 
 for index, line in enumerate(rdr):
+    qr = qrcode.QRCode(
+        version=12,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=2,
+        border=8
+    )
     # line[0]: 이름
     # line[1]: 링크
     if index != 0:
-        qr = qrcode.make(line[1])
-        qr.save("./qrcode/" + str(index) + line[0] + ".png")
+        qr.add_data(line[1])
+        qr.make()
+        img = qr.make_image(fill_color="red", back_color="#23dda0")
+        img.save("./qrcode/" + str(index) + line[0] + ".png")
 f.close()
